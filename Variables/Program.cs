ea639aac4b652edcc26f62215eb41bd17c8e2102
keviniporre <keviniporres@gmail.com>
2023-05-02 23:10:40 -0400

    Created value program
    Created a values console app

---
+++ Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'a';
+            string firstName = "Kevin";
+            bool isWorking = false;
+
+            Console.WriteLine(number);
+        }
+    }
+}
