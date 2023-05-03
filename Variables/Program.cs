57d1253341028a40276ba5c059ce35f222cd4684
keviniporre <keviniporres@gmail.com>
2023-05-02 23:14:45 -0400

    Added console log
    Console log values

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -6,6 +6,7 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            //Declare values
             byte number = 2;
             int count = 10;
             float totalPrice = 20.95f;
@@ -13,7 +14,15 @@ namespace Variables
             string firstName = "Kevin";
             bool isWorking = false;
 
+            //Shortcut for Console.WriteLine: write cw and press Tab
             Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            //Run the app: Ctrl + F5
         }
     }
 }
